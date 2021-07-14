1258586a122367f01cbc0daa7c4ba8a40ef42a15
Rebeca Gaia <reb.gaia@gmail.com>
2021-07-14 20:16:26 -0300

    consultando api cep

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import {useState} from 'react';
+
+function App(props) {
+  const [cep, setCep] = useState({});
+
+  function handleTextChange(e) {
+    fetch(`https://viacep.com.br/ws/${e.target.value}/json`)
+    .then(res => res.json())
+    .then(res => setCep(res));
+  }
+
+  return (
+    <div>
+      <strong>Olá, {props.name}</strong>
+        <div>Digite o cep: 
+          <input type="text" onChange={handleTextChange}></input>
+            {Object.entries(cep).map((values, key) => (
+              <h2 key={key}><strong>{values[0]}: {values[1]}</strong></h2>
+            ))}      
+        </div>
+    </div>
+  );
+}
+
+export default App;
